0d6950abaccf1aa2f120795e2e7e8d31a95587a2
Kasper Dyrvig <3105428+kasperdyrvig@users.noreply.github.com>
2023-10-12 20:12:49 -0200

    Backup

---
+++ sw.js
@@ -0,0 +1,32 @@
+﻿const cacheName = 'static-nuupbussii-v12';
+const urlsToCache = [
+    '.',
+    'index.html',
+    'prices.html',
+    'help.html',
+    'busstyle.css',
+    'timecalc202010.js'
+];
+
+self.addEventListener('install', event => {
+    console.log('Install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Activate event!');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
